cfc60c3b3c50bc0e9a335c8d7be2c8b630a48194
Justin Chi <j.chi2241@gmail.com>
2017-05-08 01:31:58 -0700

    add remove stock functionality, changed API

--- js/Components/App.js
+++ js/Components/App.js
@@ -8,50 +8,60 @@ import '../../css/App.scss';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this._removeStock = this._removeStock.bind(this);
+    this._addStock = this._addStock.bind(this);
     this.state = {
       dataLoaded: false,
       stocks: {}
     };
   }
+
   componentDidMount() {
     //bypass CORS policy with a proxy
     const PROXY = 'https://cors-anywhere.herokuapp.com/';
+    // const PROXY = '';
+    var alphavantageAPIKEY = 'U1KE';
+    var quandlAPIKEY = 'RLrmhU3zFMs977RD32JQ';
     var self = this;
-    var symbols = ['MSFT', 'AAPL', 'NVDA'];
-    var elements = symbols.map(function(symbol) {
+    var promises = [];
+    // var symbols = ['MSFT'];
+    var symbols = ['MSFT', 'NVDA', 'FB'];
+    // var symbols = ['MSFT', 'AAPL', 'GOOGL', 'BABA', 'AMD', 'INTU', 'FB', 'NVDA', 'S', 'BAC', 'GE', 'DIS', 'T', 'TMUS', 'ATVI'];
+    var colors = ['#FF1744', '#D500F9', '#00E676', '#00E5FF', '#FFEA00',
+      '#FF3D00', '#F50057', '#651FFF', '#1DE9B6', '#C6FF00',
+      '#3D5AFE', '#00B0FF', '#76FF03', '#FFC400'];
+
+    var series = symbols.map(function(symbol, i) {
+      var color = colors[i] ? colors[i] : colors[i % (colors.length - 1)];
       return {
-        Symbol: symbol,
-        Type: 'price',
-        Params: ['c']
+        name: symbol,
+        color: color
       }
     });
-    var series = symbols.map(function(symbol) {
-      return {
-        name: symbol
-      }
-    });
-    //stock-list data requests
-    var promises = symbols.map(function(symbol) {
-      return axios.get(PROXY + 'http://dev.markitondemand.com/Api/v2/Quote/json', {
+
+    //current quote requests
+    symbols.forEach(function(symbol) {
+      promises.push(axios.get(PROXY + 'http://dev.markitondemand.com/Api/v2/Quote/json', {
         params: {
           symbol: symbol
         }
-      });
-    });
-    //add chart data request
-    promises.push(axios.get(PROXY + 'http://dev.markitondemand.com/Api/v2/InteractiveChart/json', {
-      params: {
-        parameters: {
-          Normalized: false,
-          NumberOfDays: 1830,
-          DataPeriod: 'Day',
-          Elements: elements
+      }));
+
+      promises.push(axios.get(PROXY + 'https://www.quandl.com/api/v3/datasets/WIKI/' + symbol + '.json', {
+        params: {
+          api_key: quandlAPIKEY,
+          column_index: 4,
+          start_date: '2012-01-01'
         }
-      }
-    }));
+      }));
+    });
+
+    console.log('promises: ', promises);
+
     axios.all(promises)
       .then(function(responses) {
         responses.forEach(function(response) {
+          console.log('response: ', response)
           var data = response.data;
           if (data.LastPrice) {
           // get quote data
@@ -66,14 +76,9 @@ class App extends React.Component {
           } else {
           // get chart data
             series.forEach(function(stock) {
-              var dates = data.Dates.map(date => Date.parse(date));
-              data.Elements.forEach(function(el) {
-                series.forEach(function(stock) {
-                  if (stock.name === el.Symbol) {
-                    stock.data = el.DataSeries.close.values.map((price, i) => [dates[i], price]);
-                  }
-                });
-              });
+              if (stock.name === data.dataset.dataset_code) {
+                stock.data = data.dataset.data.map(set => [Date.parse(set[0]), set[1]]).reverse();
+              }
             });
           }
         });
@@ -81,15 +86,28 @@ class App extends React.Component {
           dataLoaded: true,
           stocks: series
         });
+        console.log('series: ', series);
       })
       .catch(error => console.log(error));
   }
+
+  _removeStock(stockToDelete) {
+    var newStocks = this.state.stocks.filter(stock => stock.name !== stockToDelete);
+    this.setState({
+      stocks: newStocks
+    });
+  }
+
+  _addStock() {
+
+  }
+
   render() {
     var components = null;
     if (this.state.dataLoaded) {
       components = (
         <div className="App">
-          <StockList stocks={this.state.stocks} />
+          <StockList stocks={this.state.stocks} _removeStock={this._removeStock} />
           <Graph stocks={this.state.stocks} />
         </div>
       );
@@ -105,7 +123,60 @@ class App extends React.Component {
   }
 }
 
+class StockList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    console.log('StockList props: ', this.props);
+    return (
+      <div className="StockList">
+        {this.props.stocks.map(function(stock, i) {
+          return <Stock stock={stock} key={i} _removeStock={this.props._removeStock}/>
+        }, this)}
+      </div>
+    );
+  }
+}
+
+class Stock extends React.Component {
+  constructor(props) {
+    super(props);
+    this._onRemoveStock = this._onRemoveStock.bind(this);
+  }
+
+  _onRemoveStock() {
+    this.props._removeStock(this.props.stock.name);
+  }
+
+  render() {
+    return (
+      <div className="Stock">
+        <div className="Stock-left">
+          <div className="Stock-symbol">{this.props.stock.name}</div>
+          <div className="Stock-fullName">{this.props.stock.fullName}</div>
+        </div>
+        <div className="Stock-right">
+          <div className="values">
+            <div className="Stock-price">{'$' + helper.round(this.props.stock.lastPrice, 2).toFixed(2)}</div>
+            <div className="Stock-changePercent">{helper.appendSign(helper.round(this.props.stock.changePercent, 2).toFixed(2)) + '%'}</div>
+          </div>
+          <div className="buttons">
+            <button className="Stock-remove-button" onClick={this._onRemoveStock}>X</button>
+            <button className="Stock-view-button">V</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
 class Graph extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   createChart() {
     Highcharts.stockChart('Graph', {
       rangeSelector: {
@@ -131,44 +202,18 @@ class Graph extends React.Component {
       series: this.props.stocks
     });
   }
+
   componentDidMount() {
     this.createChart();
   }
+
   componentDidUpdate() {
     this.createChart();
   }
+
   render() {
     return (
       <div id="Graph"></div>
-    );
-  }
-}
-
-class StockList extends React.Component {
-  render() {
-    return (
-      <div className="StockList">
-        {this.props.stocks.map(function(stock, i) {
-          return <Stock stock={stock} key={i} />
-        })}
-      </div>
-    );
-  }
-}
-
-class Stock extends React.Component {
-  render() {
-    return (
-      <div className="Stock">
-        <div className="Stock-left">
-          <div className="Stock-symbol">{this.props.stock.name}</div>
-          <div className="Stock-fullName">{this.props.stock.fullName}</div>
-        </div>
-        <div className="Stock-right">
-          <div className="Stock-price">{'$' + helper.round(this.props.stock.lastPrice, 2).toFixed(2)}</div>
-          <div className="Stock-changePercent">{helper.appendSign(helper.round(this.props.stock.changePercent, 2).toFixed(2)) + '%'}</div>
-        </div>
-      </div>
     );
   }
 }
